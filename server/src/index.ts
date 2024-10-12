b33cc56f25b4f917c978ad3cf57a9f3d3aa2619b
kiridharan <kiridharan217dec2001@gmail.com>
2024-10-12 12:53:49 +0530

    Refactor routes and add auth middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import authRoutes from "./routes/authRoutes";
 import router from "./routes/routes";
 import { AppDataSource } from "./data-source"; // Your TypeORM setup
+import { authMiddleware } from "./middleware/authMiddleware";
 
 const app = express();
 app.use(express.json()); // To parse JSON bodies
@@ -11,7 +12,7 @@ AppDataSource.initialize()
     console.log("Data source has been initialized");
 
     app.use("/api/auth", authRoutes);
-    app.use("/api", router);
+    app.use("/api", authMiddleware, router);
 
     app.listen(3000, () => {
       console.log("Server is running on port 3000");
